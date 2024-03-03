fd7252806da39176319bf6e7800c9b1e6d8bcf5b
amiftachulh <miftachul.mkb@gmail.com>
2024-03-03 19:22:10 +0700

    feat: add firebase config

---
+++ src/config/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC-73TdBnJ3OWzHGmRAz0zlTouJYuBI_pk",
+  authDomain: "hybroponic-dev.firebaseapp.com",
+  projectId: "hybroponic-dev",
+  storageBucket: "hybroponic-dev.appspot.com",
+  messagingSenderId: "15151342041",
+  appId: "1:15151342041:web:a854b37b4fda5b592479c8",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);

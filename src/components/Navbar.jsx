58752e26c3d0219419c43a2c18463441a2e919a6
Septian Agung Permana <septagung23@gmail.com>
2024-03-07 21:34:07 +0700

    UI Fix
    -Redesign Login Page
    -prefer-color-scheme: dark

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,16 +4,29 @@ import Toolbar from "@mui/material/Toolbar";
 import Button from "@mui/material/Button";
 import AdbIcon from "@mui/icons-material/Adb";
 import AccountCircle from "@mui/icons-material/AccountCircle";
-import { IconButton } from "@mui/material";
+import { IconButton, Menu, MenuItem } from "@mui/material";
 import "./Navbar.css";
+import logo from "../assets/logoIjo.svg";
 
 function ResponsiveAppBar() {
+  const [anchor, setAnchor] = React.useState(null);
+  const [open, setOpen] = React.useState(false);
+
+  const handleOpen = (e) => {
+    setAnchor(e.currentTarget);
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setAnchor(null);
+    setOpen(false);
+  };
+
   return (
     <AppBar className="navbar">
       <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
         <div style={{ display: "flex", alignItems: "center" }} className="logo">
-          <AdbIcon />
           <Button color="inherit" href="/">
+            <img src={logo} />
             Hybroponic
           </Button>
         </div>
@@ -24,9 +37,26 @@ function ResponsiveAppBar() {
           <Button color="inherit" href="/history">
             History
           </Button>
-          <IconButton>
+          <IconButton
+            id="account-menu"
+            aria-controls={open ? "basic-menu" : undefined}
+            aria-haspopup="true"
+            aria-expanded={open ? "true" : undefined}
+            onClick={handleOpen}
+          >
             <AccountCircle />
           </IconButton>
+          <Menu
+            id="account-menu"
+            anchorEl={anchor}
+            open={open}
+            onClose={handleClose}
+          >
+            <MenuItem>Profile</MenuItem>
+            <a href="/login">
+              <MenuItem>Logout</MenuItem>
+            </a>
+          </Menu>
         </div>
       </Toolbar>
     </AppBar>
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,28 +1,32 @@
-import { Button, TextField } from "@mui/material";
+import { Button } from "@mui/material";
+import logoWhite from "../assets/logoIjo.svg";
 import "./Login.css";
 
 export default function Login() {
   return (
     <div className="page">
       <div className="container">
-        <form className="login">
-          <h1>Login</h1>
-          <TextField
-            id="username"
-            label="Username"
-            variant="outlined"
-            inputProps={{ style: { textAlign: "center" } }}
-          />
-          <TextField
-            id="password"
-            label="Password"
-            variant="outlined"
-            type="password"
-          />
-          <Button variant="contained" href="/">
-            Submit
-          </Button>
-        </form>
+        <div className="form-login">
+          <form className="login">
+            <h1>Login</h1>
+            <input type="text" placeholder="Username" />
+            <input type="text" placeholder="Password" />
+            {/* <TextField id="username" label="Username" variant="outlined" /> */}
+            {/* <TextField
+              id="password"
+              label="Password"
+              variant="outlined"
+              type="password"
+            /> */}
+            <Button variant="contained" href="/">
+              Submit
+            </Button>
+          </form>
+        </div>
+        <div className="content">
+          <img src={logoWhite} style={{ width: "5rem" }} />
+          <h1>Hybroponic</h1>
+        </div>
       </div>
     </div>
   );

602052e22b8c46b4bbb97937ed841bbc5683b2fb
Septian Agung Permana <septagung23@gmail.com>
2024-03-20 21:15:26 +0700

    UI Fix Dashboard

--- src/components/ParamBox.jsx
+++ src/components/ParamBox.jsx
@@ -9,40 +9,28 @@ export default function ParamBox() {
       {/* Parameter Container */}
       <div className="paramcontainer">
         <div className="suhu kotak">
-          <div className="name">
-            <h3>Suhu</h3>
-            <h3>
-              <div className={isOn ? "on" : "off"}></div>
-              {isOn ? "On" : "Off"}
-            </h3>
-          </div>
-          <div className="nilai">
-            <h1>24&deg;C</h1>
-          </div>
+          <h3>Suhu</h3>
+          <h1>24&deg;C</h1>
+          <h3>
+            <div className={isOn ? "on" : "off"}></div>
+            {isOn ? "On" : "Off"}
+          </h3>
         </div>
         <div className="pH kotak">
-          <div className="name">
-            <h3>pH</h3>
-            <h3>
-              <div className={isOn ? "on" : "off"}></div>
-              {isOn ? "On" : "Off"}
-            </h3>
-          </div>
-          <div className="nilai">
-            <h1>6,5</h1>
-          </div>
+          <h3>pH</h3>
+          <h1>6,5</h1>
+          <h3>
+            <div className={isOn ? "on" : "off"}></div>
+            {isOn ? "On" : "Off"}
+          </h3>
         </div>
         <div className="TDS kotak">
-          <div className="name">
-            <h3>TDS</h3>
-            <h3>
-              <div className={isOn ? "on" : "off"}></div>
-              {isOn ? "On" : "Off"}
-            </h3>
-          </div>
-          <div className="nilai">
-            <h1>1050 PPM</h1>
-          </div>
+          <h3>TDS</h3>
+          <h1>1050 PPM</h1>
+          <h3>
+            <div className={isOn ? "on" : "off"}></div>
+            {isOn ? "On" : "Off"}
+          </h3>
         </div>
       </div>
 
